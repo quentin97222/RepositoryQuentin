7d0503c755e331caa224642728a2a56390a97543
Kevin Huo Yung Kai <kevin.huoyungkai@ynov.com>
2023-01-04 17:39:20 +0100

    test

--- eshopSpringv2/src/test/java/eshop/ProduitServiceTest.java
+++ eshopSpringv2/src/test/java/eshop/ProduitServiceTest.java
@@ -4,15 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import eshop.repository.FournisseurRepository;
+import eshop.config.JpaConfig;
+import eshop.repository.ClientRepository;
 
+@SpringJUnitConfig(JpaConfig.class)
 class ProduitServiceTest {
 
 //	@Autowired
 //	private ProduitService produitService;
 	@Autowired
-	private FournisseurRepository produitRepo;
+	private ClientRepository produitRepo;
 
 	@Test
 	void test() {
